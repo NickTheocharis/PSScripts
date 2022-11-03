920342bcf6cd61c940a07c15ed7a775b2ca9b374
NickTheocharis <71541663+NickTheocharis@users.noreply.github.com>
2022-11-03 09:56:00 +0200

    Add files via upload

---
+++ DeployLinuxVM.ps1
@@ -0,0 +1,20 @@
+﻿connect-azaccount
+## Create resource group
+New-AzResourceGroup -Name 'rg-dev-we-001' -Location 'WestEurope'
+## Create Linux VM
+$Credential = Get-Credential -Credential linuxadmin
+New-AzVm `
+    -ResourceGroupName 'rg-dev-we-001' `
+    -Name 'vmlinux001' `
+    -Location 'WestEurope' `
+    -Image Debian `
+    -size Standard_B2s `
+    -PublicIpAddressName pip-dev-001 `
+    -OpenPorts 80 `
+    -Credential $Credential
+   ## Create web server
+   Invoke-AzVMRunCommand `
+   -ResourceGroupName 'rg-dev-we-001' `
+   -Name 'vmlinux001' `
+   -CommandId 'RunShellScript' `
+   -ScriptString 'sudo apt-get update && sudo apt-get install -y nginx'
---
+++ DeployWindowsVM.ps1
@@ -0,0 +1,48 @@
+﻿Connect-AzAccount
+
+$Location = 'WestEurope'
+New-AzResourceGroup -Name 'rg-prod-we-001' -Location $Location
+$RG = "rg-prod-we-001"
+## Create Vnet 
+$vnet = @{
+    Name = 'vnet-prod-we-001'
+    ResourceGroupName = $RG
+    Location = $Location
+    AddressPrefix = '10.1.0.0/16'    
+}
+$Vnet = New-AzVirtualNetwork @vnet
+
+## Create subnet
+$subnet = @{
+    Name = 'snet-prod-we-001'
+    VirtualNetwork = $vnet
+    AddressPrefix = '10.1.0.0/24'
+}
+$subnetConfig = Add-AzVirtualNetworkSubnetConfig @subnet
+
+## Associate subnet to Vnet
+
+$vnet | Set-AzVirtualNetwork
+
+## Get OS Images
+
+Get-AzVMImageSku `
+   -Location $Location `
+   -PublisherName "MicrosoftWindowsServer" `
+   -Offer "WindowsServer"
+Get-AzVMImageOffer `
+   -Location $Location `
+   -PublisherName "MicrosoftWindowsServer"
+
+New-AzVm `
+    -ResourceGroupName $RG `
+    -Name 'vm-win-we-001' `
+    -Location $Location `
+    -VirtualNetworkName $Vnet `
+    -SubnetName $subnet `
+    -SecurityGroupName 'nsg-prod-we-001' `
+    -PublicIpAddressName 'pip-prod-we-001' `
+    -ImageName  "MicrosoftWindowsServer:WindowsServer:2022-datacenter-g2:latest" `
+    -OpenPorts 80,3389
+    
+    
